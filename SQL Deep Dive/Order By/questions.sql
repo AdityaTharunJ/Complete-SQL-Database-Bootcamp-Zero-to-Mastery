e2f373b6d9f84cb727f41dd5ace80ef74bf282a1
AdityaTharunJ <adityatarunjandhyala6996@gmail.com>
2026-05-26 20:24:05 +0530

    Add SQL queries for sorting employees

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,13 +3,14 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+select * from employees ORDER BY first_name ASC, last_name DESC;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
+select * from employees ORDER BY birth_date;
 
 
 /*
@@ -17,3 +18,4 @@
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select * from employees WHERE first_name ILIKE 'k%' ORDER BY hire_date
