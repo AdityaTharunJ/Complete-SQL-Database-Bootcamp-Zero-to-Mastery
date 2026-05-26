9078ab52667abd71d47122ce57f268c6204e9b0f
AdityaTharunJ <adityatarunjandhyala6996@gmail.com>
2026-05-26 20:12:15 +0530

    Update SQL queries for date filtering questions

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -4,7 +4,8 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+SELECT AGE(birth_date),* FROM employees WHERE (EXTRACT (YEAR FROM AGE(birth_date)) > 60;
+SELECT count(birth_date) FROM employees WHERE birth_date < now() - interval '61 years';
 
 /*
 * DB: Employees
@@ -12,7 +13,7 @@ SELECT * FROM employees;
 * Question: How many employees where hired in February?
 */
 
-SELECT * FROM employees;
+SELECT count(emp_no) FROM employees WHERE (EXTRACT (MONTH FROM emp_no)) = 2;
 
 /*
 * DB: Employees
@@ -20,7 +21,7 @@ SELECT * FROM employees;
 * Question: How many employees were born in november?
 */
 
-SELECT * FROM employees;
+SELECT count(birth_date) FROM employees WHERE EXTRACT(MONTH FROM birth_date) = 11;
 
 /*
 * DB: Employees
@@ -28,7 +29,7 @@ SELECT * FROM employees;
 * Question: Who is the oldest employee? (Use the analytical function MAX)
 */
 
-SELECT * FROM employees;
+SELECT max((AGE(birth_date)) FROM employees;
 
 /*
 * DB: Store
@@ -36,5 +37,5 @@ SELECT * FROM employees;
 * Question: How many orders were made in January 2004?
 */
 
-SELECT * FROM orders;
+SELECT count(orderid) FROM orders where DATE_TRUNC('month', orderdate) = date '2004-01-01';
 
