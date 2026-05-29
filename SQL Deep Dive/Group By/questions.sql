e1c82fcc39df131ef6fe6faf24cf64184f959d74
AdityaTharunJ <adityatarunjandhyala6996@gmail.com>
2026-05-29 22:15:30 +0530

    Refactor SQL queries to include GROUP BY and counts

--- SQL Deep Dive/Group By/questions.sql
+++ SQL Deep Dive/Group By/questions.sql
@@ -4,16 +4,20 @@
 *  Table: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+SELECT hire_date, count(emp_no)
+FROM employees GROUP BY hire_date
+ORDER BY "amount" asc;l
 
 /*
 *   Show me all the employees, hired after 1991 and count the amount of positions they've had
 *  Database: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+SELECT e.emp_no, count(t.titles) as "Amount of titles"
+FROM employees as e JOIN titles as t USING(emp_no)
+WHERE EXTRACT (YEAR FROM e.hire_date) > 1991
+GROUP BY e.emp_no
+ORDER BY e.emp_no;
 
 
 /*
@@ -21,5 +25,9 @@ FROM employees as e
 *  Database: Employees
 */
 
-SELECT e.emp_no
+SELECT e.emp_no, de.from_date, de.to_date
 FROM employees as e
+JOIN dept_emp as de USING(emp_no)
+WHERE de.dept_no = 'd005'
+GROUP BY e.emp_no, de.from_date, de.to_date
+ORDER BY e.emp_no, de.to_date;
